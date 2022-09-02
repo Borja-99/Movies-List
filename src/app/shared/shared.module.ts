ce600734cd0b1abde4f822e2d433282cb5bac58e
borja-capgemini <borja.estevez-penalver@capgemini.com>
2022-09-02 14:23:39 +0200

    style changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { MoviesConnexionService } from './movies-connexion.service';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MovieDetailsRoutingModule } from '../core/movie-details/movie-details-routing.module';
 
 @NgModule({
   declarations: [],
   imports: [
     HttpClientModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
     MovieDetailsRoutingModule,
+    MatIconModule
   ],
   exports: [
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [MoviesConnexionService],
   bootstrap: [],
